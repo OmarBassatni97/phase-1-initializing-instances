1c18dcc2f0692c066dbbfb80cae2443c01b48ced
omarbassatni <omarbassatni@gmail.com>
2022-08-30 21:47:07 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here
+
+class Breakfast{
+    constructor(food, drink){
+        this.food = food
+        this.drink = drink
+    }
+}
+
+new Breakfast('eggs',  'juice')
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+new Lunch('side salad', 'broccoli cheddar soup', 'iced tea')
+
+class Dinner {
+    #desert
+    constructor(salad, soup,  entree, desert){
+        this.salad = salad 
+        this.soup = soup
+        this.entree = entree
+        this.#desert = desert
+    }
+}
+
+new Dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake')
